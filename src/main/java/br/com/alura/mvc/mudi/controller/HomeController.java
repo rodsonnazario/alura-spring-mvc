f81a614324f0244f458eb3f357e0baa53fbcf4ae
rodsonnazario <rodson.nazario@gmail.com>
2022-11-22 22:20:48 -0300

    salvando pedido

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -3,6 +3,7 @@ package br.com.alura.mvc.mudi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,17 +19,6 @@ public class HomeController {
 
 	@GetMapping("/home")
 	public String home(Model model) {
-
-//		Pedido pedido = new Pedido();
-//		pedido.setNomeProduto("Pat. Skatenet Max Pink - Led Bandeirante Pink");
-//		pedido.setUrlImagem("https://m.media-amazon.com/images/I/51WEV80YBiL._AC_SX522_.jpg");
-//		pedido.setUrlProduto("https://a.co/d/dc05KtA");
-//		pedido.setDescricao("O brinquedo perfeito para as manobras mais radicais! "
-//				+ "Freio traseiro. Textura na base para maior aderência e segurança. "
-//				+ "Suspensão dianteira ajuda no desenvolvimento da coordenação motora. "
-//				+ "Rodas com luz de LED. Idade: +24 meses. Peso máximo suportado: 120kg");
-
-//		List<Pedido> pedidos = Arrays.asList(pedido);
 
 		List<Pedido> pedidos = pedidoRepository.findAll();
 		model.addAttribute("pedidos", pedidos);
